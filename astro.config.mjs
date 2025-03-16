5962f2c60c61aa4cdaecb807deef85494c17e5fd
umut <29685843+umtcndn@users.noreply.github.com>
2025-03-16 08:03:21 +0300

    blog draft true eklenmesi

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
-import pagefind from "astro-pagefind";
+import pagefind from "git";
 import tailwindcss from "@tailwindcss/vite";
 
 import vercel from "@astrojs/vercel";
