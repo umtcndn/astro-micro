ed896ca50452b4923bef5c7df41533c735fe798c
umut <29685843+umtcndn@users.noreply.github.com>
2025-03-23 06:05:25 +0300

    hata giderme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
-import pagefind from "git";
+import pagefind from "astro-pagefind";
 import tailwindcss from "@tailwindcss/vite";
 
 import vercel from "@astrojs/vercel";
